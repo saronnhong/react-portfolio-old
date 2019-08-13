37b0b58965105c1fe2d160edb745ab325a314f09
Saron <saronnhong@gmail.com>
2019-08-12 19:35:53 -0700

    Updates

---
+++ precache-manifest.274edcdf72b1f0ce334cfc3be9084c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f02a334ef8089de789f56d7e29ac06",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2f77969d5c249ea28351",
+    "url": "/react-portfolio/static/css/main.3e78ef26.chunk.css"
+  },
+  {
+    "revision": "c06d788b62fa57dcd00d",
+    "url": "/react-portfolio/static/js/2.c187eb58.chunk.js"
+  },
+  {
+    "revision": "2f77969d5c249ea28351",
+    "url": "/react-portfolio/static/js/main.050d8fa1.chunk.js"
+  },
+  {
+    "revision": "4d1d151b896bea087de4",
+    "url": "/react-portfolio/static/js/runtime~main.3f94e198.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.465efdf71ba78bda5101f71d24b80c0b.js"
+  "/react-portfolio/precache-manifest.274edcdf72b1f0ce334cfc3be9084c32.js"
 );
 
 self.addEventListener('message', (event) => {
