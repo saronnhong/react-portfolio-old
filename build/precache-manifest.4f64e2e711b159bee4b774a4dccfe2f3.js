53c1afee9abccc02e5da9646c75718b58a0c778a
Saron <saronnhong@gmail.com>
2019-08-14 12:00:31 -0700

    remove some assignments from portfolio section

--- build/precache-manifest.4f64e2e711b159bee4b774a4dccfe2f3.js
+++ build/precache-manifest.4f64e2e711b159bee4b774a4dccfe2f3.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4654f02d593b938d346e3d199875c177",
+    "revision": "9b4c9b5119426bf8ef45e2a201962f76",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "74b0226a74227f4cddd9",
-    "url": "/react-portfolio/static/css/main.86fd9473.chunk.css"
+    "revision": "7e3b79344fa007484e7d",
+    "url": "/react-portfolio/static/css/main.64d647cd.chunk.css"
   },
   {
     "revision": "ae52efae5fdaeb9abee2",
     "url": "/react-portfolio/static/js/2.8f8c17ec.chunk.js"
   },
   {
-    "revision": "74b0226a74227f4cddd9",
+    "revision": "7e3b79344fa007484e7d",
     "url": "/react-portfolio/static/js/main.4576bbc9.chunk.js"
   },
   {
--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
                 <div className="row">
                   <div className="aboutMeInfo">
                     <p>I am a Full Stack Web Developer with strong skills in HTML, CSS, Javascript, NodeJS, Express, MongoDB, and React. I’m passionate about learning new things and find great satisfaction in solving complex problems. </p>
-                    <p>My previous career as a Software Test Engineer allowed me to understand the hard work it takes to put out quality software and the importance of identifying bugs in the early stages of software development. Applying this unique perspective (not sure if it’s unique but it might be) and diverse skills to Web Development has helped me improve the quality of my applications and save time in the debugging/testing phase of development.</p>
+                    <p>My previous career as a Software Test Engineer allowed me to understand the hard work it takes to put out quality software and the importance of identifying bugs in the early stages of software development. Applying this unique perspective and diverse skills to Web Development has helped me improve the quality of my applications and save time in the debugging phase of development.</p>
                   </div>
                 </div>
               </div>
