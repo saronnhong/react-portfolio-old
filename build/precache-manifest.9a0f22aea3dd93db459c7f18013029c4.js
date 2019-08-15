2b1be4cc76f701f908de38072556b92d7eff212e
Saron <saronnhong@gmail.com>
2019-08-15 15:30:22 -0700

    remove underline navbar

--- build/precache-manifest.9a0f22aea3dd93db459c7f18013029c4.js
+++ build/precache-manifest.9a0f22aea3dd93db459c7f18013029c4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "864091644495109b24cdcdda9b277a8a",
+    "revision": "4516a162677baebdb01949ef8ab28cda",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "eadae4809fe966d90ba6",
-    "url": "/react-portfolio/static/css/main.64d647cd.chunk.css"
+    "revision": "12796fed1d515277a7b2",
+    "url": "/react-portfolio/static/css/main.ef2ae275.chunk.css"
   },
   {
     "revision": "ae52efae5fdaeb9abee2",
     "url": "/react-portfolio/static/js/2.8f8c17ec.chunk.js"
   },
   {
-    "revision": "eadae4809fe966d90ba6",
-    "url": "/react-portfolio/static/js/main.c489392f.chunk.js"
+    "revision": "12796fed1d515277a7b2",
+    "url": "/react-portfolio/static/js/main.688c290d.chunk.js"
   },
   {
     "revision": "4d1d151b896bea087de4",
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,25 +7,6 @@ function Navbar(props) {
     return (
         <div className="navTop">
             <nav className="navbar navbar-dark bg-dark fixed-top navbar-expand-sm" id="nav1">
-                {/* <div className="navDiv ">
-
-                    <Link smooth to="/react-portfolio">
-                        <div className="navbar-brand" ><i class="far fa-address-card"></i> Saron Nhong</div>
-                    </Link>
-
-                    <Link smooth to="/react-portfolio/#contact">
-                        <div className="navMenu" id="navContact">Contact</div>
-                    </Link>
-                    <Link smooth to="/react-portfolio/portfolio">
-                        <div className="navMenu" href="#portSec" id="navPort">Portfolio</div>
-                    </Link>
-
-                    <Link smooth to="/react-portfolio/#aboutMe">
-                        <div className="navMenu" id="navAbout"> About</div>
-                    </Link>
-
-
-                </div> */}
                 <Link smooth to="/react-portfolio">
                     <div className="navbar-brand" ><i class="far fa-address-card"></i> Saron Nhong</div>
                 </Link>
@@ -34,10 +15,9 @@ function Navbar(props) {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-
                         <li class="nav-item">
                             <Link smooth to="/react-portfolio/#aboutMe">
-                                <div className="navMenu" id="navAbout"> About</div>
+                                <div className="navMenu" id="navAbout">About</div>
                             </Link>
                         </li>
                         <li class="nav-item">
@@ -45,8 +25,7 @@ function Navbar(props) {
                                 <div className="navMenu" href="#portSec" id="navPort">Portfolio</div>
                             </Link>
                         </li>
-
-                        <li class="nav-item active">
+                        <li class="nav-item">
                             <Link smooth to="/react-portfolio/#contact">
                                 <div className="navMenu" id="navContact">Contact</div>
                             </Link>
