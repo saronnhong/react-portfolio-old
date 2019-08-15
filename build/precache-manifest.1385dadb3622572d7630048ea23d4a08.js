36f29f7cef95a2ea51baa887e0d83501b4484f4a
Saron <saronnhong@gmail.com>
2019-08-15 16:15:12 -0700

    add media queries for homepage

--- build/precache-manifest.1385dadb3622572d7630048ea23d4a08.js
+++ build/precache-manifest.1385dadb3622572d7630048ea23d4a08.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f6095ef4c28999a72c7156722a38cc0",
+    "revision": "65b7bc54d794f6119709ec4017a04a82",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "7d782516e5ff954b2e84",
-    "url": "/react-portfolio/static/css/main.505e36db.chunk.css"
+    "revision": "5fbf91fef715914999f4",
+    "url": "/react-portfolio/static/css/main.a71dd102.chunk.css"
   },
   {
     "revision": "ae52efae5fdaeb9abee2",
     "url": "/react-portfolio/static/js/2.8f8c17ec.chunk.js"
   },
   {
-    "revision": "7d782516e5ff954b2e84",
-    "url": "/react-portfolio/static/js/main.fbd70edb.chunk.js"
+    "revision": "5fbf91fef715914999f4",
+    "url": "/react-portfolio/static/js/main.8684eb5f.chunk.js"
   },
   {
     "revision": "4d1d151b896bea087de4",
