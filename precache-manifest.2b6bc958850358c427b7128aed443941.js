2dcc4b3e6dad80fce64d583f0f9ff14cbadd8fb9
Saron <saronnhong@gmail.com>
2019-08-15 16:28:10 -0700

    Updates

--- precache-manifest.2b6bc958850358c427b7128aed443941.js
+++ precache-manifest.2b6bc958850358c427b7128aed443941.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2907903c961e7bc584faa6bb42c4e88e",
+    "revision": "e3b682928d3e378a8bd47f0147bba9ac",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "7c7dcbcad5998dc7e97f",
-    "url": "/react-portfolio/static/css/main.83d75e01.chunk.css"
+    "revision": "c7b48ed8b3e323c23877",
+    "url": "/react-portfolio/static/css/main.944b5ec8.chunk.css"
   },
   {
     "revision": "ae52efae5fdaeb9abee2",
     "url": "/react-portfolio/static/js/2.8f8c17ec.chunk.js"
   },
   {
-    "revision": "7c7dcbcad5998dc7e97f",
+    "revision": "c7b48ed8b3e323c23877",
     "url": "/react-portfolio/static/js/main.8684eb5f.chunk.js"
   },
   {
