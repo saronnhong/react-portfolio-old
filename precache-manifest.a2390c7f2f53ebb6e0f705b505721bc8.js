e705fdb15755448968026dd2837d1e790ddd48c9
Saron <saronnhong@gmail.com>
2019-08-15 15:30:59 -0700

    Updates

--- precache-manifest.a2390c7f2f53ebb6e0f705b505721bc8.js
+++ precache-manifest.a2390c7f2f53ebb6e0f705b505721bc8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4516a162677baebdb01949ef8ab28cda",
+    "revision": "2f6095ef4c28999a72c7156722a38cc0",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "12796fed1d515277a7b2",
-    "url": "/react-portfolio/static/css/main.ef2ae275.chunk.css"
+    "revision": "7d782516e5ff954b2e84",
+    "url": "/react-portfolio/static/css/main.505e36db.chunk.css"
   },
   {
     "revision": "ae52efae5fdaeb9abee2",
     "url": "/react-portfolio/static/js/2.8f8c17ec.chunk.js"
   },
   {
-    "revision": "12796fed1d515277a7b2",
-    "url": "/react-portfolio/static/js/main.688c290d.chunk.js"
+    "revision": "7d782516e5ff954b2e84",
+    "url": "/react-portfolio/static/js/main.fbd70edb.chunk.js"
   },
   {
     "revision": "4d1d151b896bea087de4",
